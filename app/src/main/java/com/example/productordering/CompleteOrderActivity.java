bf4ebb3248bf236284b54d948466cbe45979289e
Dharian Malchan <dharianmalchan@gmail.com>
2021-10-19 11:36:04 -0400

    Added Serializable Fixed

--- app/src/main/java/com/example/productordering/CompleteOrderActivity.java
+++ app/src/main/java/com/example/productordering/CompleteOrderActivity.java
@@ -2,28 +2,26 @@ package com.example.productordering;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.ComponentName;
+
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.util.ArrayList;
 
 public class CompleteOrderActivity extends AppCompatActivity {
 
     public static final String EXTRA_ORDERS = "Orders";
-    public static final String filename = "order_file.ser";
+    public static final String filename = "order_file.dat";
+    public static final String PAST_ORDERS = "Past_Orders";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,43 +38,39 @@ public class CompleteOrderActivity extends AppCompatActivity {
             orders.add((Order) bundle.getSerializable(key));
         }
 
+        Context context = getApplicationContext();
         TextView textView = (TextView) findViewById(R.id.completed_order);
         StringBuilder list_orders = new StringBuilder();
 
-        for (Order order : orders) {
-            list_orders.append(order.toString());
-            list_orders.append("\n");
-        }
 
             // Serialization
             try
             {
 
-                FileOutputStream file = new FileOutputStream(filename);
+                FileOutputStream file = context.openFileOutput(filename, Context.MODE_PRIVATE);
                 ObjectOutputStream out = new ObjectOutputStream(file);
 
                 for (Order order : orders) {
                     list_orders.append(order.toString());
                     list_orders.append("\n");
 
-                    out.writeObject(orders);
+                    out.writeObject(order);
                 }
 
                 out.close();
                 file.close();
 
-                Context context = getApplicationContext();
                 CharSequence text = "Object has been serialized";
                 int duration = Toast.LENGTH_SHORT;
 
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
 
+                textView.setText(list_orders.toString());
             }
 
             catch(IOException ex)
             {
-                Context context = getApplicationContext();
                 CharSequence text = "IOException is caught";
                 int duration = Toast.LENGTH_SHORT;
 
@@ -84,14 +78,10 @@ public class CompleteOrderActivity extends AppCompatActivity {
                 toast.show();
             }
 
-
-        textView.setText(list_orders.toString());
     }
 
 
     public void onSendOrder(View view){
-
-
 
         TextView textView = (TextView) findViewById(R.id.completed_order);
         String text = textView.getText().toString();
@@ -102,6 +92,76 @@ public class CompleteOrderActivity extends AppCompatActivity {
         sendIntent.setType("text/plain");
         startActivity(sendIntent);
 
+
+    }
+
+    public void onDisplayOrder(View view){
+
+        ArrayList<Order> orders = new ArrayList<>();
+        Order object1 =  null;
+
+        Context context = getApplicationContext();
+        // Deserialization
+        try
+        {
+            // Reading the object from a file
+            FileInputStream file = context.openFileInput(filename);
+            ObjectInputStream in = new ObjectInputStream(file);
+
+            // Method for deserialization of object
+
+           while (file.available()!=0) {
+
+               object1 = (Order) in.readObject();
+               if (object1!=null) {
+                   orders.add(object1);
+               }
+               else{
+                   break;
+               }
+            }
+
+            in.close();
+            file.close();
+
+
+            StringBuilder list_orders = new StringBuilder();
+
+            for (Order order : orders) {
+                list_orders.append(order.toString());
+                list_orders.append("\n");
+            }
+
+            Intent intent = new Intent(this, DisplayOrderActivity.class);
+            intent.putExtra(PAST_ORDERS, list_orders.toString());
+
+            CharSequence text = "Object has been deserialized";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+            startActivity(intent);
+
+        }
+
+        catch(IOException ex)
+        {
+            CharSequence text = "IOException is caught";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+
+        catch(ClassNotFoundException ex)
+        {
+            CharSequence text = "ClassNotFoundException is caught";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
 
     }
 
--- app/src/main/java/com/example/productordering/DisplayOrderActivity.java
+++ app/src/main/java/com/example/productordering/DisplayOrderActivity.java
@@ -3,6 +3,7 @@ package com.example.productordering;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,58 +20,11 @@ public class DisplayOrderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_order);
 
-        Order object1 =  null;
+        Intent intent = getIntent();
+        String past_Orders = intent.getStringExtra(CompleteOrderActivity.PAST_ORDERS);
+        TextView textView = (TextView) findViewById(R.id.display_order);
+        textView.setText(past_Orders);
 
-        // Deserialization
-        try
-        {
-            // Reading the object from a file
-            FileInputStream file = new FileInputStream(CompleteOrderActivity.filename);
-            ObjectInputStream in = new ObjectInputStream(file);
-
-            // Method for deserialization of object
-            object1 = (Order) in.readObject();
-
-            in.close();
-            file.close();
-
-            TextView textView = (TextView) findViewById(R.id.completed_order);
-            StringBuilder list_orders = new StringBuilder();
-
-
-         /*   list_orders.append(object1.toString());
-            list_orders.append("\n");
-
-            textView.setText(list_orders); */
-
-            Context context = getApplicationContext();
-            CharSequence text = "Object has been deserialized";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-
-        }
-
-        catch(IOException ex)
-        {
-            Context context = getApplicationContext();
-            CharSequence text = "IOException is caught";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        }
-
-        catch(ClassNotFoundException ex)
-        {
-            Context context = getApplicationContext();
-            CharSequence text = "ClassNotFoundException is caught";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        }
     }
 
 
